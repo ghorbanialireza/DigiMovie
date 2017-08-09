04ea6304f118a5808241184c577ee41e70b500a9
ghorbanialireza <alireza67ghorbani@gmail.com>
2017-08-10 00:12:20 +0430

    primary

---
+++ DigiMovie/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+            //return Content("salam:");
+            //return RedirectToAction("About");
+             //return HttpNotFound();
+            //return new EmptyResult();
+            //return View("About");
+            //return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+        }
+        
+        public ActionResult About(/*int? id,string message*/)
+        {
+            //if (id == null)
+
+            //    return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+
+            //return Content(id.ToString() + "<hr>" + message);
+
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ DigiMovie/Controllers/tempController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiMovie.Controllers
+{
+    public class tempController : Controller
+    {
+        // GET: temp
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("gallery/{year:int:range(1300,1400)}/{month:int:range(1,12)}/{message}")]
+        public ActionResult Show(int year, int month, string message)
+        {
+            return Content(year + "<br>" + month + "<br>" + message);
+        }
+    }
+}
